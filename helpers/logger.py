f4789c6ad4846cebaf476bac564d368da6e9a19b
Joao Luiz Severo <joaoluizseverot@gmail.com>
2026-06-21 15:59:47 -0300

    feat(helpers): add utilitary scripts folder with python logger script

---
+++ helpers/logger.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import logging
+from loguru import logger
+
+
+LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
+
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        logger.opt(depth=6, exception=record.exc_info).log(
+            record.levelname, record.getMessage()
+        )
+
+logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
+
+
+logger.add(
+    sys.stdout,
+    format="<green>[{time:YYYY-MM-DD HH:mm:ss}][<level>{level}</level>]</green> {name}:{function} -> {message}",
+    level=LOG_LEVEL
+)
